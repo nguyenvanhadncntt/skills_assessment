0067b3745cb8a94d51540aa8344a23acca2c1b87
nguyenvanhadncntt <nguyenvanhadncntt@gmail.com>
2024-05-10 00:11:16 +0700

    update db design

--- group-service/src/main/java/com/learning/skill/assessment/group/GroupApplication.java
+++ group-service/src/main/java/com/learning/skill/assessment/group/GroupApplication.java
@@ -1,4 +1,4 @@
-package com.learning.skill.assessment;
+package com.learning.skill.assessment.group;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
