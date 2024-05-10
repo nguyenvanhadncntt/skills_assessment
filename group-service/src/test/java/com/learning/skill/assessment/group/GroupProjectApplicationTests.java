2385772793059a57a9008e2eb5e120ef8baf72b9
nguyenvanhadncntt <nguyenvanhadncntt@gmail.com>
2024-05-10 22:37:45 +0700

    config for jacoco report

---
+++ group-service/src/test/java/com/learning/skill/assessment/group/GroupProjectApplicationTests.java
@@ -0,0 +1,11 @@
+package com.learning.skill.assessment.group;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupProjectApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
