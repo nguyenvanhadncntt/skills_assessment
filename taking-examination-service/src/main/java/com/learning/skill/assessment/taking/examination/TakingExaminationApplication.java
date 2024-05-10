2bbdb220bd24c55fc670508daffd6878e9535017
nguyenvanhadncntt <nguyenvanhadncntt@gmail.com>
2024-05-10 15:38:33 +0700

    fix some error build

--- taking-examination-service/src/main/java/com/learning/skill/assessment/taking/examination/TakingExaminationApplication.java
+++ taking-examination-service/src/main/java/com/learning/skill/assessment/taking/examination/TakingExaminationApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TakingExaminationApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TakingTestApplication.class, args);
+        SpringApplication.run(TakingExaminationApplication.class, args);
     }
 }
